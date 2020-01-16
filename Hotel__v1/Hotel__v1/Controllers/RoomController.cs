40590b9fad9d4026c140f4918c4be7edda05a5b0
Dawid <Dawidos01111@gmail.com>
2020-01-16 19:22:07 +0100

    add RoomControllers.cs

--- Hotel__v1/Hotel__v1/Controllers/RoomController.cs
+++ Hotel__v1/Hotel__v1/Controllers/RoomController.cs
@@ -9,9 +9,30 @@ namespace Hotel__v1.Controllers
     public class RoomController : Controller
     {
         // GET: Room
+        private HotelDBEntities objHotelDbEntities;
+
+        public RoomController()
+        {
+            objHotelDbEntities = new HotelDBEntities();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            RoomViewModel objRoomViewModel = new RoomViewModel();
+            objRoomViewModel.ListOfBookingStatus = (from obj in objHotelDbEntities.BookingStatus
+                                                    select new SelectListItem()
+                                                    {
+Text = obj.BookingStatus,
+Valur = obj.BookingStatusId.ToString()
+                                                    }).toList();
+
+            objRoomViewModel.ListOfRoomType = (from obj in objHotelDbEntities.RoomTypes
+                                               select new SelectListItem()
+                                               {
+                                                   Text = obj.RoomTypeName,
+                                                   Valur = obj.RoomTypeId.ToString()
+                                               }).toList();
+            return View(objRoomViewModel);
         }
     }
 }
