e9bb3304c4b47c46cb002808753b90abaddcc7f6
AgusQ85 <34649420+AgusQ85@users.noreply.github.com>
2020-01-16 14:23:37 +0100

    14:23 - czekam na uruchomienie BD

---
+++ Hotel__v1/Hotel__v1/ViewModel/RoomViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel__v1.ViewModel
+{
+    public class RoomViewModel
+    {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public string RommImage { get; set; }
+        public decimal RoomPrice { get; set; }
+        public int BookingStatusId { get; set; }
+        public int RoomTypeId { get; set; }
+        public int RoomCapacity { get; set; }
+        public HttpPostedFileBase Image { get; set; }
+        public string RoomDescription { get; set; }
+        public List<SelectListItem> ListOfBookingStatus { get; set; }
+        public List<SelectListItem> ListOfRoomType { get; set;}
+
+    }
+}
